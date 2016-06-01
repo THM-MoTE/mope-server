e4115b2b454205a1d25a9e266fbbbd521d882f07
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 16:05:42 +0200

    Implement /connect command

--- src/main/scala/de/thm/moie/server/Routes.scala
+++ src/main/scala/de/thm/moie/server/Routes.scala
@@ -1,28 +1,30 @@
 package de.thm.moie.server
 
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.model.ContentTypes
 import akka.pattern.ask
-import akka.util.Timeout
-
-import scala.concurrent.duration._
-import scala.sys.process._
-import scala.concurrent.Future
-import java.io._
-
-import akka.actor.Props
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
 import de.thm.moie.project.ProjectDescription
+import de.thm.moie.project.ProjectDescription._
+import de.thm.moie.server.ProjectsManagerActor.ProjectId
+
+import scala.concurrent.ExecutionContext
 
 trait Routes extends JsonSupport {
+  this: ServerSetup =>
+
+  def projectsManager: ActorRef
+
   val routes =
     pathPrefix("moie") {
       path("connect") {
         post {
           entity(as[ProjectDescription]) { description =>
-            println(description)
-            complete("subscribed")
+            complete {
+              for {
+                id <- (projectsManager ? description).mapTo[ProjectId]
+              } yield IntJsonFormat.write(id.id)
+            }
           }
         } ~ get {
           complete(ProjectDescription("Dummy-URL", List()))
