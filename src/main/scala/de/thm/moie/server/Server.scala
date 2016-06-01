b08e792e6c5cb5923fde950431833448321604ee
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 14:06:18 +0200

    Create server-class

---
+++ src/main/scala/de/thm/moie/server/Server.scala
@@ -0,0 +1,35 @@
+package de.thm.moie.server
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.ContentTypes
+import scala.concurrent.Await
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.sys.process._
+import scala.concurrent.Future
+import java.io._
+import akka.actor.Props
+
+class Server(port:Int) extends Routes {
+
+  private val escCode = 0x1b
+
+  implicit val actorSystem = ActorSystem("moie-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val context = actorSystem.dispatcher
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", port)
+  println(s"Server running at localhost/$port")
+  println("Press Enter to interrupt")
+  var char = StdIn.readLine()
+
+  bindingFuture.
+    flatMap(_.unbind()).
+    onComplete(_ => actorSystem.terminate())
+}
