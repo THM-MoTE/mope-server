456cce57782130e93470e62ff138976000330b92
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 16:06:53 +0200

    Depend on ServerSetup

--- src/main/scala/de/thm/moie/server/Server.scala
+++ src/main/scala/de/thm/moie/server/Server.scala
@@ -1,28 +1,30 @@
 package de.thm.moie.server
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
 import scala.io.StdIn
 import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.model.ContentTypes
+
 import scala.concurrent.Await
 import akka.pattern.ask
 import akka.util.Timeout
+
 import scala.concurrent.duration._
 import scala.sys.process._
 import scala.concurrent.Future
 import java.io._
-import akka.actor.Props
 
-class Server(port:Int) extends Routes {
+import de.thm.moie.project.ProjectDescription
+
+class Server(port:Int) extends Routes with ServerSetup {
 
   private val escCode = 0x1b
 
-  implicit val actorSystem = ActorSystem("moie-system")
-  implicit val materializer = ActorMaterializer()
-  implicit val context = actorSystem.dispatcher
+  override lazy val projectsManager: ActorRef = actorSystem.actorOf(Props[ProjectsManagerActor], name = "Root-ProjectsManager")
 
   val bindingFuture = Http().bindAndHandle(routes, "localhost", port)
   println(s"Server running at localhost:$port")
