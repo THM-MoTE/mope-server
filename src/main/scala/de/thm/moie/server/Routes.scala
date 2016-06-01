d855f00722cd0c9748d2f4775156de3b7a21df1b
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 14:06:29 +0200

    Create main-routes

---
+++ src/main/scala/de/thm/moie/server/Routes.scala
@@ -0,0 +1,20 @@
+package de.thm.moie.server
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.ContentTypes
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.sys.process._
+import scala.concurrent.Future
+import java.io._
+import akka.actor.Props
+
+trait Routes {
+  val routes =
+    path("connect") {
+      complete("connected stub")
+    }
+}
