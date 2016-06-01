7bb89fac81597edf2cd76d6d6ef0349be3435ba2
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 13:02:57 +0200

    Create Config, ConfigLoader

---
+++ src/main/scala/de/thm/moie/config/Config.scala
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
+ */
+
+package de.thm.moie.config
+
+trait Config {
+  def getAll: List[(String,String)]
+  def getString(key:String):Option[String]
+  def getInt(key:String):Option[Int] =
+    try {
+      getString(key).map(_.toInt)
+    } catch {
+      case _:NumberFormatException => None
+    }
+
+  def getDouble(key:String): Option[Double] =
+    try {
+      getString(key).map(_.toDouble)
+    } catch {
+      case _:NumberFormatException => None
+    }
+
+  def getFloat(key:String): Option[Float] = getDouble(key).map(_.toFloat)
+  def getBoolean(key:String): Option[Boolean] = getString(key).map {
+    case "true" => true
+    case _ => false
+  }
+}
---
+++ src/main/scala/de/thm/moie/config/ConfigLoader.scala
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
+ */
+
+package de.thm.moie.config
+
+import java.net.URL
+
+import scala.io.Source
+
+class ConfigLoader(path:URL) extends Config {
+  private val map =
+    Source.fromURL(path, "UTF-8").getLines().flatMap { line =>
+      if(line.startsWith("#")) List()
+      else {
+        val splitted = line.split("=")
+        if(splitted.length != 2) List()
+        else List( (splitted.head.trim, splitted.tail.head.trim) )
+      }
+    }.toMap
+
+  override def getAll: List[(String,String)] = map.toList
+  override def getString(key:String):Option[String] = map.get(key)
+}
