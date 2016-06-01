d57de89ce4c57e10a3db3bddebf630f20ad0d444
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 14:06:39 +0200

    Create a server-instance in main-method

--- src/main/scala/de/thm/moie/MoIE.scala
+++ src/main/scala/de/thm/moie/MoIE.scala
@@ -1,7 +1,12 @@
 package de.thm.moie
 
+import de.thm.moie.server.Server
+
+
 object MoIE {
   def main(args:Array[String]) = {
     println("loaded")
+    val port = Global.config.getInt("server-port").getOrElse(9000)
+    val server = new Server(port)
   }
 }
