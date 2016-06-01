0612be4f4fc8e6d8da0679d7966726066f9b1553
N. Justus <nicola.justus@mni.thm.de>
2016-06-01 16:07:31 +0200

    Create helper UnhandledReceiver

---
+++ src/main/scala/de/thm/moie/utils/actors/UnhandledReceiver.scala
@@ -0,0 +1,17 @@
+package de.thm.moie.utils.actors
+
+import akka.actor.Actor
+
+trait UnhandledReceiver {
+  this: Actor =>
+
+  private val actorName = this.self.path.name
+
+  def catchUnhandledMsgs: Actor.Receive = {
+    case a:Any => println(s"WARNING: $actorName can't handle $a")
+  }
+
+  def handleMsg: Receive
+
+  override def receive: Receive = handleMsg.orElse(catchUnhandledMsgs)
+}
