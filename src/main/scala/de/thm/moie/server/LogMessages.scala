f792e1b8c606565a03cf54429ab10d28f232c4b7
N. Justus <nicola.justus@mni.thm.de>
2016-06-02 10:13:52 +0200

    Create LogMessages for logging all received msgs

---
+++ src/main/scala/de/thm/moie/server/LogMessages.scala
@@ -0,0 +1,15 @@
+package de.thm.moie.server
+
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.event.Logging
+
+trait LogMessages extends Actor {
+  val log = Logging(context.system, this)
+
+  override abstract def receive: Receive = {
+    case a:Any =>
+      log.info(s"Msg: $a")
+      super.receive(a)
+  }
+}
