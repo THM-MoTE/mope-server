ec3366cebbbbdde8af52fd85fe8352585916dd9e
N. Justus <nicola.justus@mni.thm.de>
2016-06-02 17:34:53 +0200

    Explicit define type for json-formatter
    (Works as a hint to the compiler so that he uses the right implicits.)

--- src/main/scala/de/thm/moie/server/JsonSupport.scala
+++ src/main/scala/de/thm/moie/server/JsonSupport.scala
@@ -1,9 +1,11 @@
 package de.thm.moie.server
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import de.thm.moie.compiler.CompilerError
 import de.thm.moie.project.ProjectDescription
-import spray.json.DefaultJsonProtocol
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val projectDescriptionFormat = jsonFormat2(ProjectDescription)
+  implicit val projectDescriptionFormat: RootJsonFormat[ProjectDescription] = jsonFormat3(ProjectDescription)
+  implicit val compileErrorFormat:RootJsonFormat[CompilerError] = jsonFormat4(CompilerError)
 }
