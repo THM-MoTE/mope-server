18584770892b28404c20072651d29c99e05811ec
N. Justus <nicola.justus@mni.thm.de>
2016-06-05 15:14:10 +0200

    Track project dependencies in one place

--- build.sbt
+++ build.sbt
@@ -29,12 +29,4 @@ mainClass in assembly := (mainClass in Compile).value
 assemblyJarName in assembly := s"${name.value}-${version.value}.jar"
 test in assembly := {} //skip test's during packaging
 
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-http-core" % "2.4.6",
-  "com.typesafe.akka" %% "akka-http-experimental" % "2.4.6",
-  "com.typesafe.akka" %% "akka-http-spray-json-experimental" % "2.4.6",
-  "com.typesafe.akka" %% "akka-testkit" % "2.4.6",
-  "org.scalactic" %% "scalactic" % "2.2.6",
-  "org.scalatest" %% "scalatest" % "2.2.6" % "test",
-  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
-  )
+libraryDependencies ++= Dependencies.usedDependencies
---
+++ project/Dependencies.scala
@@ -0,0 +1,25 @@
+import sbt._
+
+object Dependencies {
+  private val akkaVersion = "2.4.6"
+  private val akkaGroup = "com.typesafe.akka"
+  val akka = Seq(
+    akkaGroup %% "akka-http-core" % akkaVersion,
+    akkaGroup %% "akka-http-experimental" % akkaVersion,
+    akkaGroup %% "akka-http-spray-json-experimental" % akkaVersion,
+    akkaGroup %% "akka-testkit" % akkaVersion
+  )
+
+  private val scalaUtilsVersion = "1.0.4"
+  private val scalaUtilsGroup = "org.scala-lang.modules"
+  val scalaUtils = Seq(
+     scalaUtilsGroup %% "scala-parser-combinators" % scalaUtilsVersion
+  )
+
+  val testLib = Seq(
+      "org.scalactic" %% "scalactic" % "2.2.6",
+      "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+  )
+
+  val usedDependencies = akka ++ scalaUtils ++ testLib
+}
