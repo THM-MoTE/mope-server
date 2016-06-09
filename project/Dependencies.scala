3513e7e763213b009f8f95d0d0d89dbd94170b3e
N. Justus <nicola.justus@mni.thm.de>
2016-06-09 11:20:50 +0200

    Add akka-http-testkit as dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,7 @@ object Dependencies {
     akkaGroup %% "akka-http-core" % akkaVersion,
     akkaGroup %% "akka-http-experimental" % akkaVersion,
     akkaGroup %% "akka-http-spray-json-experimental" % akkaVersion,
+    akkaGroup %% "akka-http-testkit" % akkaVersion,
     akkaGroup %% "akka-testkit" % akkaVersion,
     akkaGroup %% "akka-slf4j" % akkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.1.3"
