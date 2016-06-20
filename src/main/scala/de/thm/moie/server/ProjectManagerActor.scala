6374812737cec1fd249c08f3f3ebe97faa9f4109
N. Justus <nicola.justus@mni.thm.de>
2016-06-20 15:58:29 +0200

    Index files and compile only if they change

--- src/main/scala/de/thm/moie/server/ProjectManagerActor.scala
+++ src/main/scala/de/thm/moie/server/ProjectManagerActor.scala
@@ -11,6 +11,7 @@ import akka.pattern.{ask, pipe}
 import akka.util.Timeout
 import de.thm.moie.compiler.{CompilerError, ModelicaCompiler}
 import de.thm.moie.project.ProjectDescription
+import de.thm.moie.server.FileWatchingActor.{DeletedPath, GetFiles, ModifiedPath, NewPath}
 import de.thm.moie.utils.actors.UnhandledReceiver
 
 import scala.collection._
@@ -28,20 +29,40 @@ class ProjectManagerActor(description:ProjectDescription,
 
   implicit val timeout = Timeout(5 seconds)
 
-  //initialize all files
   val rootDir = Paths.get(description.path)
-  val fileWatchingActor = context.actorOf(Props(new FileWatchingActor(rootDir, description.outputDirectory)))
+  val fileWatchingActor = context.actorOf(Props(new FileWatchingActor(self, rootDir, description.outputDirectory)))
+
+  private val projectFiles = mutable.ArrayBuffer[Path]()
+  private val compileErrors = mutable.ArrayBuffer[CompilerError]()
+
+  override def preStart() =
+    for {
+      files <- (fileWatchingActor ? GetFiles).mapTo[List[Path]]
+      errors <- compiler.compileAsync(files).map(_.filter(errorInProjectFile))
+    } {
+      self ! InitialInfos(files, errors)
+    }
 
   def errorInProjectFile(error:CompilerError): Boolean =
     Paths.get(error.file).startsWith(rootDir)
 
   override def handleMsg: Receive = {
+    case InitialInfos(files, errors) =>
+      projectFiles.clear()
+      compileErrors.clear()
+      projectFiles ++= files
+      compileErrors ++= errors
+      log.debug("new init infos: files {} errors {}", projectFiles, compileErrors)
+      context become initialized
+  }
+
+  private def initialized: Receive = {
     case CompileProject =>
-      (for {
-        files <- (fileWatchingActor ? FileWatchingActor.GetFiles).mapTo[List[Path]]
-        errors <- compiler.compileAsync(files).map(_.filter(errorInProjectFile))
-        _ = printDebug(errors)
-      } yield errors) pipeTo sender
+
+      compileErrors.toSeq
+    case ModifiedPath(p) => log.debug("path modified {}", p)
+    case NewPath(p) => log.debug("path new {}", p)
+    case DeletedPath(p) => log.debug("path delete {}", p)
   }
 
   private def printDebug(errors:Seq[CompilerError]): Unit = {
@@ -58,4 +79,8 @@ class ProjectManagerActor(description:ProjectDescription,
 object ProjectManagerActor {
   sealed trait ProjectManagerMsg
   case object CompileProject extends ProjectManagerMsg
+  private[ProjectManagerActor] case class InitialInfos(files:Seq[Path], errors:Seq[CompilerError])
+
+  //all hold infos about a modelica file
+  case class ModelicaInfo(errors:Seq[CompilerError])
 }
