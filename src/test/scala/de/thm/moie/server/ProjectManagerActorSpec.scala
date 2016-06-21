6fb373fd99ec97b1e94bac24093c5b5223d23b49
N. Justus <nicola.justus@mni.thm.de>
2016-06-21 12:00:26 +0200

    Add test for compiling project

--- src/test/scala/de/thm/moie/server/ProjectManagerActorSpec.scala
+++ src/test/scala/de/thm/moie/server/ProjectManagerActorSpec.scala
@@ -8,8 +8,12 @@ import akka.actor.Props
 import akka.testkit.{ TestActors, TestActorRef, TestKit, ImplicitSender, TestProbe }
 import de.thm.moie.project.ProjectDescription
 import de.thm.moie._
+import de.thm.moie.server.ProjectManagerActor._
 import de.thm.moie.compiler._
 import java.nio.file._
+import java.nio.charset.StandardCharsets
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class ProjectManagerActorSpec
   extends ActorSpec {
@@ -17,6 +21,11 @@ class ProjectManagerActorSpec
   val path = Files.createTempDirectory("moie")
   val projectPath = path.resolve("mo-project")
 
+  val testFile = projectPath.resolve("test.mo")
+
+  override def beforeAll = {
+    Files.createDirectory(projectPath)
+  }
 
   override def afterAll = {
     super.afterAll()
@@ -43,6 +52,29 @@ class ProjectManagerActorSpec
       xs.filter(manager.errorInProjectFile) shouldEqual List(projectPath.resolve("test/util.mo"),
         projectPath.resolve("test/util3.mo"),
         projectPath.resolve("project.mo")).map(dummyError)
+    }
+  }
+
+  "ProjectManager" must {
+    "return compile errors for existing files" in {
+        //write file content
+      val contentWithErrors = """
+      |model myModel
+      |   Rl number;
+      |end myModel;
+      """.stripMargin
+
+      Files.createFile(testFile)
+      val bw = Files.newBufferedWriter(testFile, StandardCharsets.UTF_8,
+        StandardOpenOption.CREATE,
+        StandardOpenOption.TRUNCATE_EXISTING)
+      bw.write(contentWithErrors)
+      bw.close()
+
+        //test errors
+      testRef ! CompileProject
+      val xs = expectMsgType[List[CompilerError]](5 seconds)
+      xs.size should be (1)
     }
   }
 }
