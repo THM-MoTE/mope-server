822f284dd0d0e883f6ea62070a47a38e45ed41f0
N. Justus <nicola.justus@mni.thm.de>
2016-06-30 15:38:47 +0200

    Compile with a specific script if project contains package.mo files

--- src/main/scala/de/thm/moie/compiler/OMCompiler.scala
+++ src/main/scala/de/thm/moie/compiler/OMCompiler.scala
@@ -3,25 +3,47 @@
  */
 
 package de.thm.moie.compiler
-import java.nio.file.{Files, Path}
+import java.nio.file.{Files, Path, StandardOpenOption}
 
 import de.thm.moie.utils.ProcessExitCodes
 import de.thm.moie.utils.ProcessUtils._
-
-import scala.util._
-import scala.sys.process.Process
-
+import de.thm.moie.utils.ResourceUtils._
 import org.slf4j.LoggerFactory
+
+import scala.sys.process.Process
+import scala.util._
 
 class OMCompiler(compilerFlags:List[String], executableName:String, outputDir:String) extends ModelicaCompiler {
   private val log = LoggerFactory.getLogger(this.getClass)
   private val msgParser = new MsgParser()
 
   private val moScriptSwitch = "--showErrorMessages"
+  private val tmpScriptName = "compile.mos"
+
+  private val isPackageMo:Path => Boolean = _.endsWith("package.mo")
+
+  def sortPathes(paths:List[Path]): List[Path] =
+    paths.map(p => p.getParent -> p).sorted.map(_._2)
 
   override def compile(files: List[Path]): Seq[CompilerError] = {
     val pathes = files.map(_.toAbsolutePath.toString)
     files.headOption match {
+      case Some(path) if files.exists(isPackageMo) =>
+        //TODO: HACK: use length of path to find shortest; shortest file is in root directory
+        //FIX THIS BEFORE WITH BREADTH-FIRST ordering #18
+        val rootProjectFile =
+          files.map(_.toAbsolutePath).
+          filter(isPackageMo).
+          sortBy(p => p.toString.size).head
+        //generate a script to compile
+        val outputDir = createOutputDir(rootProjectFile.getParent)
+        val scriptPath = generateTmpScript(outputDir, rootProjectFile)
+        //exec omc & parse result
+        val compilerExec = executableName :: compilerFlags ::: List(scriptPath.toAbsolutePath.toString)
+        val cmd = Process(compilerExec, outputDir.toFile)
+        val (status, stdout, _) = runCommand(cmd)
+//        log.debug(s"run tmpScript with\n$stdout")
+        parseErrorMsg(stdout)
       case Some(path) =>
         val outputDir = createOutputDir(path.getParent)
         val compilerExec = executableName :: (compilerFlags ::: pathes)
@@ -55,4 +77,19 @@ class OMCompiler(compilerFlags:List[String], executableName:String, outputDir:St
       Files.createDirectory(outputPath)
     outputPath
   }
+
+  private def generateTmpScript(outputPath:Path, rootProjectFile:Path): Path = {
+    val scriptPath = outputPath.resolve(tmpScriptName)
+    tryR(Files.newBufferedWriter(
+      scriptPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+      bw =>
+        val content =
+          s"""loadFile("${rootProjectFile}");
+             |getErrorString();""".stripMargin
+        bw.write(content)
+        log.debug(s"compile-script written to $scriptPath")
+        scriptPath
+    }
+  }
+
 }
