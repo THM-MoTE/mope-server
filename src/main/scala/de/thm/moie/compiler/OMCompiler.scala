b488353abad35a8dc598880fb3f56b65f7ca6750
N. Justus <nicola.justus@mni.thm.de>
2016-07-06 15:22:55 +0200

    Remove deprecated code

--- src/main/scala/de/thm/moie/compiler/OMCompiler.scala
+++ src/main/scala/de/thm/moie/compiler/OMCompiler.scala
@@ -6,7 +6,6 @@ package de.thm.moie.compiler
 import java.nio.file.{Files, Path, StandardOpenOption}
 
 import de.thm.moie.utils.MonadImplicits._
-import de.thm.moie.utils.ResourceUtils._
 import omc.corba.ScriptingHelper._
 import omc.corba._
 import org.slf4j.LoggerFactory
@@ -25,16 +24,12 @@ class OMCompiler(compilerFlags:List[String], executableName:String, outputDir:Pa
     case e:Exception => log.error("Couldn't initialize omc connection", e)
   }
 
-  private val moScriptSwitch = "--showErrorMessages"
-  private val tmpScriptName = "compile.mos"
-
   private val isPackageMo:Path => Boolean = _.endsWith("package.mo")
 
   def sortPathes(paths:List[Path]): List[Path] =
     paths.map(p => p.getParent -> p).sorted.map(_._2)
 
   override def compile(files: List[Path]): Seq[CompilerError] = {
-    val pathes = files.map(_.toAbsolutePath.toString)
     files.headOption match {
       case Some(path) if files.exists(isPackageMo) =>
         //generate a script to compile
@@ -61,21 +56,15 @@ class OMCompiler(compilerFlags:List[String], executableName:String, outputDir:Pa
     val fileList = asStringArray(files.asJava)
     val expr = s"""loadFiles($fileList)"""
     val res = omc.sendExpression(expr)
-    log.debug("loadFiles() returned {}", res)
     val errOpt:Option[String] = res.error
     errOpt.map(parseErrorMsg).getOrElse(Seq[CompilerError]())
   }
 
   override def compileScript(path:Path): Seq[CompilerError] = {
-    compileScript(path, List(moScriptSwitch))
-  }
-
-  private def compileScript(path:Path, compilerFlags:List[String]): Seq[CompilerError] = {
     val startDir = path.getParent
     withOutputDir(startDir) {
       omc.sendExpression("clear()")
       val resScript = omc.sendExpression(s"""runScript(${asString(path)})""")
-      log.debug("runScript returned {}", resScript.result)
       parseResult(resScript)
     }
   }
@@ -96,19 +85,6 @@ class OMCompiler(compilerFlags:List[String], executableName:String, outputDir:Pa
   private def createOutputDir(path:Path): Unit = {
     if(!Files.exists(path))
       Files.createDirectory(path)
-  }
-
-  private def generateTmpScript(outputPath:Path, rootProjectFile:Path): Path = {
-    val scriptPath = outputPath.resolve(tmpScriptName)
-    tryR(Files.newBufferedWriter(
-      scriptPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-      bw =>
-        val content =
-          s"""loadFile(${asString(rootProjectFile)});
-             |getErrorString();""".stripMargin
-        bw.write(content)
-        scriptPath
-    }
   }
 
   private def withOutputDir(dir: Path)(f: => Seq[CompilerError]): Seq[CompilerError] = {
