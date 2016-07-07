2e7cffa57091cbf1fa42b160673a025a67664e38
N. Justus <nicola.justus@mni.thm.de>
2016-07-07 13:15:29 +0200

    Create compileProject.sh

---
+++ tools/compileProject.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+force=$1
+SERVER="../moie-server"
+OMC_API="../omc-java-api"
+EWS="../EnhancedWatchService"
+
+function compileSbt() {
+  if [ $force = "true" ]
+  then
+    sbt ";clean;compile"
+  else
+    sbt compile
+  fi
+}
+function compileGradle() {
+  if [ $force = "true" ]
+  then
+    gradle clean compileJava
+  else
+    gradle compileJava
+  fi
+}
+
+echo "compiling " $EWS
+cd $EWS
+compileSbt
+compileGradle
+
+echo
+echo "compiling " $OMC_API
+cd $OMC_API
+compileSbt
+compileGradle
+
+echo
+echo "compiling " $SERVER
+cd $SERVER
+compileSbt
