60b7c48e30f406290641cd24ab012441fc101b8e
N. Justus <nicola.justus@mni.thm.de>
2016-07-11 11:14:12 +0200

    Rewrite ProjectManagerActorSpec

--- src/test/scala/de/thm/moie/server/ProjectManagerActorSpec.scala
+++ src/test/scala/de/thm/moie/server/ProjectManagerActorSpec.scala
@@ -5,13 +5,15 @@
 package de.thm.moie.server
 
 import org.scalatest._
-import akka.testkit.{ TestActorRef }
+import akka.testkit.TestActorRef
 import de.thm.moie.project.ProjectDescription
 import de.thm.moie._
 import de.thm.moie.server.ProjectManagerActor._
 import de.thm.moie.compiler._
 import java.nio.file._
 import java.nio.charset.StandardCharsets
+
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -21,12 +23,7 @@ class ProjectManagerActorSpec
   val path = Files.createTempDirectory("moie")
   val projectPath = path.resolve("mo-project")
 
-  val testFile = projectPath.resolve("test.mo")
-  val scriptFile = projectPath.resolve("script.mos")
-
   Files.createDirectory(projectPath)
-  Files.createFile(testFile)
-  Files.createFile(scriptFile)
 
   override def afterAll = {
     super.afterAll()
@@ -37,7 +34,7 @@ class ProjectManagerActorSpec
 
   private def dummyError(x:Path) = CompilerError("Error", x.toAbsolutePath().toString(), FilePosition(0,0), FilePosition(0,0), "")
 
-  val testRef = TestActorRef[ProjectManagerActor](new ProjectManagerActor(stubDescription, new OMCompiler(List(), "omc", projectPath.resolve(stubDescription.outputDirectory))))
+  val testRef = TestActorRef[ProjectManagerActor](new ProjectManagerActor(stubDescription, new OMCompiler(List(), "omc", projectPath.resolve(stubDescription.outputDirectory)), false))
   val manager = testRef.underlyingActor
 
   "ProjectManager's `errorInProjectFile`" should {
@@ -56,7 +53,6 @@ class ProjectManagerActorSpec
         projectPath.resolve("test/util3.mo"),
         projectPath.resolve("project.mo")).map(dummyError)
     }
-
   }
 
   "ProjectManager" must {
@@ -68,118 +64,74 @@ class ProjectManagerActorSpec
     )
     createdMoFiles.foreach(Files.createFile(_))
 
-    val allMoFiles = testFile :: createdMoFiles
+    val allMoFiles = createdMoFiles
     val deletedFiles = allMoFiles.take(2)
     val remainingFiles = allMoFiles.drop(2)
 
-    "return 1 compile error for invalid file" in {
-        //write file content
-      val contentWithErrors = """
-      |model myModel
-      |   Rel number
-      |end myModel;
-      """.stripMargin
-
-      val bw = Files.newBufferedWriter(testFile, StandardCharsets.UTF_8,
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING)
-      bw.write(contentWithErrors)
-      bw.write("\n")
-      bw.close()
-
-      Thread.sleep(1000) //give OS time to throw a CREATE event
-
-        //test errors
-      testRef ! CompileProject(testFile)
-      val xs = expectMsgType[List[CompilerError]](10 seconds)
-      xs.size should be (1)
-    }
-
-    "return 0 compile errors for valid file" in {
-        //write file content
-      val contentWithErrors = """
-      |model myModel
-      |   Real number;
-      |end myModel;
-      """.stripMargin
-
-      val bw = Files.newBufferedWriter(testFile, StandardCharsets.UTF_8,
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING)
-      bw.write(contentWithErrors)
-      bw.write("\n")
-      bw.close()
-
-      Thread.sleep(1000) //wait till buffers are written
-
-        //test errors
-      testRef ! CompileProject(testFile)
-      val xs = expectMsgType[List[CompilerError]](10 seconds)
-      xs.size should be (0)
-    }
-
-    "return compile errors for invalid scripts" in {
-      val scriptContent = """
-lodFile("bouncing_ball.mo");
-""".stripMargin
-
-            val bw = Files.newBufferedWriter(scriptFile, StandardCharsets.UTF_8,
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING)
-      bw.write(scriptContent)
-      bw.write("\n")
-      bw.close()
-
-      Thread.sleep(1000) //wait till buffers are written
-
-        //test errors
-      testRef ! CompileScript(scriptFile)
-      val xs = expectMsgType[List[CompilerError]](10 seconds)
-      xs.size should be (1)
-    }
-
-    "return 0 compile errors for valid scripts" in {
-      val scriptContent = """
-loadFile("bouncing_ball.mo");
-""".stripMargin
-
-      val bw = Files.newBufferedWriter(scriptFile, StandardCharsets.UTF_8,
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING)
-      bw.write(scriptContent)
-      bw.write("\n")
-      bw.close()
-
-      Thread.sleep(1000) //wait till buffers are written
-
-      //test errors
-      testRef ! CompileScript(scriptFile)
-      val xs = expectMsgType[List[CompilerError]](10 seconds)
-      xs.size should be (0)
-    }
-
     "contain only existent files" in {
-      Thread.sleep(8000) //wait a bit; until all OS events are processed
       val fileList = manager.getProjectFiles
-      fileList.toSet shouldEqual allMoFiles.toSet
+      val files = Await.result(fileList, 10 seconds)
+      files.toSet shouldEqual allMoFiles.toSet
     }
 
     "contain existent files which are sorted" in {
-      // Thread.sleep(10000) //wait a bit; until all OS events are processed
       val fileList = manager.getProjectFiles
-      fileList shouldEqual allMoFiles.sorted
+      val files = Await.result(fileList, 10 seconds)
+      files shouldEqual allMoFiles.sorted
     }
 
     "remove files if they got deleted" in {
       deletedFiles.foreach(Files.delete(_))
-      Thread.sleep(8000)
+      Thread.sleep(5000)
       val fileList = manager.getProjectFiles
-      fileList.toSet shouldEqual remainingFiles.toSet
+      val files = Await.result(fileList, 10 seconds)
+      files.toSet shouldEqual remainingFiles.toSet
     }
 
     "contain sorted files even if some file got deleted" in {
       val fileList = manager.getProjectFiles
-      fileList shouldEqual remainingFiles.sorted
+      val files = Await.result(fileList, 10 seconds)
+      files shouldEqual remainingFiles.sorted
+    }
+
+    "return 1 compile error for invalid file" in {
+      val invalidFile = createInvalidFile(projectPath)
+
+      //test errors
+      require(Files.exists(invalidFile), "file has to be created!")
+//      Thread.sleep(20000)
+      val files = manager.getProjectFiles
+      println("files in test: "+files)
+      testRef ! CompileProject(invalidFile)
+      val xs = expectMsgType[List[CompilerError]](10 seconds)
+      xs.size shouldBe 1
+      xs.head shouldBe invalidFileError(invalidFile)
+      Files.deleteIfExists(invalidFile)
+    }
+
+    "return 0 compile errors for valid file" in {
+      val validFile = createValidFile(projectPath)
+        //test errors
+      testRef ! CompileProject(validFile)
+      val xs = expectMsgType[List[CompilerError]](10 seconds)
+      xs shouldBe empty
+    }
+
+    "return compile errors for invalid scripts" in {
+      val scriptFile = createInvalidScript(projectPath)
+        //test errors
+      testRef ! CompileScript(scriptFile)
+      val xs = expectMsgType[List[CompilerError]](10 seconds)
+      xs.size shouldBe 1
+      xs.head shouldBe invalidScriptError(scriptFile)
+    }
+
+    "return 0 compile errors for valid scripts" in {
+      val validScriptFile = createValidScript(projectPath)
+      //test errors
+      testRef ! CompileScript(validScriptFile)
+      val xs = expectMsgType[List[CompilerError]](10 seconds)
+      xs shouldBe empty
     }
   }
 }
