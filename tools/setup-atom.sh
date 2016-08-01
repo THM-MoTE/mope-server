e486e953d5e596fb75744eb937c5e7054422953a
N. Justus <nicola.justus@mni.thm.de>
2016-08-01 16:48:01 +0200

    Remove useless echo

--- tools/setup-atom.sh
+++ tools/setup-atom.sh
@@ -3,7 +3,6 @@
 #links the atom-plugin package into the atom editor
 
 if which apm > /dev/null; then
-    echo "Install dependencies"
     apm install
     echo "Link plugin into packages"
     apm link
