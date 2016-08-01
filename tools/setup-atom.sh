208bf19870bd8aa8b0923c1dca9b2f1fe2765bcc
N. Justus <nicola.justus@mni.thm.de>
2016-08-01 16:14:34 +0200

    Add script for linking the atom plugin

---
+++ tools/setup-atom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#links the atom-plugin package into the atom editor
+
+if which apm > /dev/null; then
+    echo "Install dependencies"
+    apm install
+    echo "Link plugin into packages"
+    apm link
+else
+    echo "Can't find `apm`-command; is it on your $PATH?"
+fi
