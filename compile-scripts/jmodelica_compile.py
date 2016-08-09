b894f4e6c951fa8edac7403f66dc14ce2b809d98
N. Justus <nicola.justus@mni.thm.de>
2016-08-09 17:20:37 +0200

    Add outputdir to cli args

--- compile-scripts/jmodelica_compile.py
+++ compile-scripts/jmodelica_compile.py
@@ -5,12 +5,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", nargs="+")
     parser.add_argument("classname", nargs=1)
+    parser.add_argument("outputdir", nargs=1)
 
     args = parser.parse_args()
     files = args.file[1:]
     className = args.classname
     print "files ", files
     print "classname ", className
+    print "output ", parser.outputdir
     #pymodelica.compile_fmu(className, files)
 
 main()
