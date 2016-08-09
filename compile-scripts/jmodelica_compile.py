adf6ac8e400c2918fe0d2fe3171b5f1cd26b08cb
N. Justus <nicola.justus@mni.thm.de>
2016-08-09 17:57:24 +0200

    Use real script flags

--- compile-scripts/jmodelica_compile.py
+++ compile-scripts/jmodelica_compile.py
@@ -3,9 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", nargs="+")
-    parser.add_argument("classname", nargs=1)
-    parser.add_argument("outputdir", nargs=1)
+    parser.add_argument("-classname", nargs=1)
+    parser.add_argument("-outputdir", nargs=1)
+    parser.add_argument("-file", nargs="+")
 
     args = parser.parse_args()
     files = args.file[1:]
