6393df60603992c823ecf21c8ecd816febdb3596
N. Justus <nicola.justus@mni.thm.de>
2016-08-09 18:00:13 +0200

    Add info-string to cli args

--- compile-scripts/jmodelica_compile.py
+++ compile-scripts/jmodelica_compile.py
@@ -4,9 +4,9 @@ import os.path as path
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-classname", nargs=1)
-    parser.add_argument("-outputdir", nargs=1)
-    parser.add_argument("-file", nargs="+")
+    parser.add_argument("-classname", nargs=1, help="Name of the class, which should get instantiated")
+    parser.add_argument("-outputdir", nargs=1, help="Directory for generated files")
+    parser.add_argument("-file", nargs="+", help="The Modelica Source files")
 
     args = parser.parse_args()
     files = [x for x in args.file if path.exists(x)]
