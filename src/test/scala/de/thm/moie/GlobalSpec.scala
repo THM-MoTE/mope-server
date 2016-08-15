27d33ce47751a3f4bfa357f91b8bef33bbf90ba2
N. Justus <nicola.justus@mni.thm.de>
2016-08-15 13:19:08 +0200

    Add test for ApplicationMode.parseString

--- src/test/scala/de/thm/moie/GlobalSpec.scala
+++ src/test/scala/de/thm/moie/GlobalSpec.scala
@@ -20,4 +20,20 @@ class GlobalSpec extends FlatSpec with Matchers {
     words2.head shouldBe "Boolean"
     words2(1) shouldBe "Integer"
   }
+
+  "ApplicationMode.parseString" should "parse mode options" in {
+    val erg1 = Global.ApplicationMode.parseString("dev")
+    val erg2 = Global.ApplicationMode.parseString("DEVELOPMENT")
+    val erg3 = Global.ApplicationMode.parseString("prod")
+    val erg4 = Global.ApplicationMode.parseString("PRODUCTION")
+
+    erg1 shouldBe Global.ApplicationMode.Development
+    erg2 shouldBe Global.ApplicationMode.Development
+    erg3 shouldBe Global.ApplicationMode.Production
+    erg4 shouldBe Global.ApplicationMode.Production
+  }
+
+  an [IllegalArgumentException] should be thrownBy {
+    Global.ApplicationMode.parseString("dav")
+  }
 }
