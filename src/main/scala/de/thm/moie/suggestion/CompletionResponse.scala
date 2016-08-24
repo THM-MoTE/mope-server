10ead3ff27f6651d46683e9afd23f057c23d98e0
N. Justus <nicola.justus@mni.thm.de>
2016-08-24 10:09:29 +0200

    Add CompletionType.Property

--- src/main/scala/de/thm/moie/suggestion/CompletionResponse.scala
+++ src/main/scala/de/thm/moie/suggestion/CompletionResponse.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,6 @@ case class CompletionResponse(completionType: CompletionType.Value,
 
 object CompletionResponse {
   object CompletionType extends Enumeration {
-    val Type, Variable, Function, Keyword, Package, Model, Class = Value
+    val Type, Variable, Function, Keyword, Package, Model, Class, Property = Value
   }
 }
