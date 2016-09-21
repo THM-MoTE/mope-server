04caea87a64ceca441eebea238f5745c5f27933d
N. Justus <nicola.justus@mni.thm.de>
2016-09-21 14:12:39 +0200

    Replace - separated config-key with camel case

--- src/main/scala/de/thm/moie/compiler/OMCompiler.scala
+++ src/main/scala/de/thm/moie/compiler/OMCompiler.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -35,7 +35,7 @@ class OMCompiler(executableName:String, outputDir:Path) extends ModelicaCompiler
   private val log = LoggerFactory.getLogger(this.getClass)
   private val msgParser = new MsgParser()
   private val omc: OMCInterface = {
-    val forceEnglish = Global.config.getBoolean("force-english")
+    val forceEnglish = Global.config.getBoolean("forceEnglish")
     if(forceEnglish) new OMCClient(executableName, Global.usLocale)
     else new OMCClient(executableName)
   }
--- src/main/scala/de/thm/moie/server/ValidateConfig.scala
+++ src/main/scala/de/thm/moie/server/ValidateConfig.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,7 +28,7 @@ trait ValidateConfig {
   def validateConfig(config:Config): Errors = {
     val buffer = scala.collection.mutable.ArrayBuffer[String]()
     val compilerKey = "compiler"
-    val compilerExecKey = "compiler-executable"
+    val compilerExecKey = "compilerExecutable"
 
     if(!config.hasPath(compilerKey)) { //check which compiler
       buffer += s"`$compilerKey` is undefined. Specify which compiler to use. For example: $compilerKey=omc"
