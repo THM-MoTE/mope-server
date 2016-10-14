7242bea672bd42c28dd89c5cb6bbe060b1505061
N. Justus <nicola.justus@mni.thm.de>
2016-10-14 14:14:24 +0200

    Fix tests for DocumentationProvider

--- src/test/scala/de/thm/mope/doc/DocumentationProviderSpec.scala
+++ src/test/scala/de/thm/mope/doc/DocumentationProviderSpec.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -62,17 +62,6 @@ class DocumentationProviderSpec extends ActorSpec {
     "return no documentation for non-existent classes" in {
       testRef ! GetDocumentation("nico")
       expectMsg(5 seconds, None)
-    }
-
-    "return classComment from stdlib" in {
-      val exp = ClassComment("Modelica.Electrical", "Library of electrical models (analog, digital, machines, multi-phase)")
-      testRef ! GetClassComment("Modelica.Electrical")
-      expectMsg(Some(exp))
-    }
-
-    "return no classComment for non-existent classes" in {
-      testRef ! GetClassComment("Modelica.nico")
-      expectMsg(None)
     }
   }
 }
