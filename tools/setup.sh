723d6e0f751e0974e176d798169bddb1b24b3bfd
N. Justus <nicola.justus@mni.thm.de>
2016-11-06 13:02:12 +0100

    Add linking atom-plugin to setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -6,20 +6,34 @@ server=moie-server
 
 mkdir $root
 cd $root
-echo "Cloning server repos"
+echo "===> Cloning server repos"
 git clone git@git.thm.de:njss90/moie-server.git
 git clone git@git.thm.de:njss90/omc-java-api.git
 git clone https://github.com/njustus/EnhancedWatchService.git
 
-echo "Cloning atom plugin"
+echo
+echo "===> Cloning atom plugin"
 git clone "git@git.thm.de:njss90/moie-atom-plugin.git"
 
 cd $server
 sbt compile
 
 echo
-echo "Projects cloned & compiled! They are at:"
+echo "===> Projects cloned & compiled! They are at:"
 echo "./"$root"/moie-server"
 echo "./"$root"/omc-java-api"
 echo "./"$root"/EnhancedWatchService"
 echo "./"$root"/moie-atom-plugin"
+echo
+echo "================"
+echo "===> Now installing Atom-Plugin into Atom"
+
+cd ../moie-atom-plugin
+if which apm > /dev/null; then
+    apm install
+    echo "Link plugin into packages"
+    apm link
+		echo "Plugin installed!"
+else
+    echo "Can't find `apm`-command; is it on your $PATH?"
+fi
