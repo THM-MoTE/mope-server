334ca4e39bff4230178873683f75d2c5e31944b8
N. Justus <nicola.justus@mni.thm.de>
2016-12-05 12:01:26 +0100

    Move global types into 'move' package object #97

--- src/main/scala/de/thm/mope/compiler/ModelicaCompiler.scala
+++ src/main/scala/de/thm/mope/compiler/ModelicaCompiler.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,6 +19,7 @@ package de.thm.mope.compiler
 
 import java.nio.file.Path
 
+import de.thm.mope._
 import de.thm.mope.declaration.JumpToLike
 import de.thm.mope.doc.DocumentationLike
 import de.thm.mope.suggestion.CompletionLike
@@ -31,7 +32,7 @@ trait ModelicaCompiler
     with JumpToLike
     with DocumentationLike {
 
-  protected val isPackageMo:Path => Boolean = _.endsWith("package.mo")
+  protected val isPackageMo:PathFilter = _.endsWith("package.mo")
 
   /** Disconnects this compiler from his backend. */
   def stop(): Unit
