b8c62d9e4b2af5fd3d8e362fe23c216f1ab55f71
N. Justus <nicola.justus@mni.thm.de>
2017-02-03 17:52:44 +0100

    Allow \/ as path delimiter :scream_cat: #110

--- src/main/scala/de/thm/mope/compiler/CommonParsers.scala
+++ src/main/scala/de/thm/mope/compiler/CommonParsers.scala
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -34,7 +34,7 @@ trait CommonParsers extends RegexParsers with ImplicitConversions {
   def unknownError:String = "Compiler didn't provide any further message"
   def unknownPath:String = ""
 
-  def pathDelimiter = """(?:/|\\)""".r
+  def pathDelimiter = """(?:/|\\|\\/)""".r
   def pathIdent = """[a-z-A-Z-.+<>0-9?=_ ]+""".r
   def number = """[0-9]+""".r
   def ident =  """[a-zA-Z0-9]+""".r
