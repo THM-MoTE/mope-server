0ed53a67dadf0b658f634323af64f960e327321d
N. Justus <nicola.justus@mni.thm.de>
2017-02-07 14:36:19 +0100

    Update repo links to github #105

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 # downloads the subprojects and compiles the project
 
-root=moie
-server=moie-server
+root=mope
+server=mope-server
 
 mkdir $root
 cd $root
 echo "===> Cloning server repos"
-git clone git@git.thm.de:njss90/moie-server.git
-git clone git@git.thm.de:njss90/omc-java-api.git
-git clone https://github.com/njustus/EnhancedWatchService.git
+git clone https://github.com/THM-MoTE/mope-server.git
+git clone https://github.com/THM-MoTE/omc-java-api.git
+git clone https://github.com/THM-MoTE/EnhancedWatchService.git
 
 echo
 echo "===> Cloning atom plugin"
-git clone "git@git.thm.de:njss90/moie-atom-plugin.git"
+git clone https://github.com/THM-MoTE/mope-atom-plugin.git
+
+cd "omc-java-api"
+sbt compile
 
 cd $server
 sbt compile
 
 echo
 echo "===> Projects cloned & compiled! They are at:"
-echo "./"$root"/moie-server"
+echo "./"$root"/mope-server"
 echo "./"$root"/omc-java-api"
 echo "./"$root"/EnhancedWatchService"
-echo "./"$root"/moie-atom-plugin"
+echo "./"$root"/mope-atom-plugin"
 echo
 echo "================"
 echo "===> Now installing Atom-Plugin into Atom"
 
-cd ../moie-atom-plugin
+cd ../mope-atom-plugin
 if which apm > /dev/null; then
     apm install
     echo "Link plugin into packages"
