866b1eec0e0e73e45f7c9981d234b8b00ad76f9b
N. Justus <nicola.justus@mni.thm.de>
2017-09-27 17:25:20 +0200

    Revert "Create abstract LspRoutes"
    This reverts commit 6ec25f5f1bcccb431e108ce2856ee8b404082885.

--- src/main/scala/de/thm/mope/lsp/Routes.scala
+++ src/main/scala/de/thm/mope/lsp/Routes.scala
@@ -2,8 +2,11 @@ package de.thm.mope.lsp
 import akka.stream.scaladsl._
 import de.thm.mope.server.JsonSupport
 
-trait Routes extends LspRoutes {
-  def routes: RpcMethod[_,_] =
-    ("compile" -> Flow[Int].map(_*2)) |
-    ("complete" -> Flow[String].map(_.toUpperCase))
+trait Routes extends JsonSupport {
+  def routes = (RpcMethod("compile")(
+    Flow[Int].map(_*2)
+  ) | RpcMethod("complete")(
+    Flow[String].map(_.toUpperCase)
+  )
+  )
 }
