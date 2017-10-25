ae970e3c28cde23f0511a03ed0c31883475a7cf5
N. Justus <nicola.justus@mni.thm.de>
2017-10-25 11:18:31 +0200

    Move fallback & akka config into conf/fallback.conf

--- src/main/scala/de/thm/mope/config/FallbackConfig.scala
+++ src/main/scala/de/thm/mope/config/FallbackConfig.scala
@@ -21,14 +21,5 @@ import com.typesafe.config.ConfigFactory
 import scala.collection.JavaConverters._
 
 trait FallbackConfig {
-  private val fallbackMap =
-    Map("force-english" -> "false",
-      "indexFiles" -> "true",
-      "exitOnLastDisconnect" -> "false",
-      "app.mode" -> "prod",
-      "defaultAskTimeout" -> "20",
-      "http.interface" -> "127.0.0.1",
-      "http.port" -> "9001")
-
-  val fallbackConfig = ConfigFactory.parseMap(fallbackMap.asJava)
+  val fallbackConfig = ConfigFactory.parseResources("fallback.conf")
 }
