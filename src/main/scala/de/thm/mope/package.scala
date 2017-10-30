d5fe9804d843b402fb9608ff9e361eb6c07dcafe
N. Justus <nicola.justus@mni.thm.de>
2017-10-30 16:40:56 +0100

    Update :copyright: headers

--- src/main/scala/de/thm/mope/package.scala
+++ src/main/scala/de/thm/mope/package.scala
@@ -1,19 +1,20 @@
-package de.thm
+/**
+  * Copyright (C) 2016,2017 Nicola Justus <nicola.justus@mni.thm.de>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
-import java.nio.file.Path
-
-import akka.actor.{ActorRef, Props}
-import com.softwaremill.tagging._
-import de.thm.mope.declaration.JumpToLike
-import de.thm.mope.doc.DocumentationLike
-import de.thm.mope.project.ProjectDescription
-import de.thm.mope.suggestion.{CompletionLike, PrefixMatcher, SrcFileInspector}
-
-package object mope {
-  type Filter[A] = A => Boolean
-  type PathFilter = Filter[Path]
-
-  import tags._
 
   //factories
   type Factory[A] = () => A
