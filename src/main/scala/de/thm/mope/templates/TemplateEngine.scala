bd5bf786967d12f4d2e5534c8e8b188b5af554e3
N. Justus <nicola.justus@mni.thm.de>
2018-02-27 15:53:30 +0100

    Revert "Update :copyright: headers"
    This reverts commit d5fe9804d843b402fb9608ff9e361eb6c07dcafe.

--- src/main/scala/de/thm/mope/templates/TemplateEngine.scala
+++ src/main/scala/de/thm/mope/templates/TemplateEngine.scala
@@ -1,5 +1,5 @@
 /**
-  * Copyright (C) 2016,2017 Nicola Justus <nicola.justus@mni.thm.de>
+  * Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
-
 
 package de.thm.mope.templates
 
